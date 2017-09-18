e8906503e64384487fe0c833b0739497a801cbf4
John <872050787@qq.com>
2017-09-18 09:55:53 +0800

    新增学生签到主界面的button

--- ex-modules/student/stuSignBtn/stuSignBtnDirective.js
+++ ex-modules/student/stuSignBtn/stuSignBtnDirective.js
@@ -6,6 +6,7 @@ angular.module("student")
             restrict: "AE",
             replace: true,
             controller: "stuSignBtnCtl",
+            require:"^stuMain",
             templateUrl: "ex-modules/student/stuSignBtn/stuSignBtnTemplate.html",
             link: function (scope, el, attrs, ctrl) {
 
@@ -13,6 +14,10 @@ angular.module("student")
 
                 setCourseState(now);
 
+                /**
+                 * 设置当前课程状态
+                 * @param now
+                 */
                 function setCourseState(now) {
                     var nowTime = now.getTime();
                     console.log(scope.getCourseTime(scope.course.begintime).getTime());
@@ -21,63 +26,70 @@ angular.module("student")
                         scope.signState = "未开始";
                     }
                     else if(nowTime>scope.getCourseTime(scope.course.begintime).getTime()&&nowTime<scope.getCourseTime(scope.course.endtime).getTime()){
-                        scope.signState = "迟到";
+                        //scope.signState = "迟到";
+                        scope.state = 2;
                     }
                     else if(nowTime>scope.getCourseTime(scope.course.endtime).getTime()){
                         scope.signState = "缺勤";
+                        scope.state = 3;
                     }
                     else if(nowTime>=scope.getCourseTime(scope.course.begintime)-15*60*1000 && nowTime<=scope.getCourseTime(scope.course.begintime)){
-                        scope.signState = "正常签到";
+                        scope.signState = "待签到";
+                        scope.state = 0;
                     }
-                    $(el).find("button").on("click",function () {
-                        var now =  new Date();
-                        var hours = "";
-                        var minute = "";
-                        var signintime = now.getYear()+"-"+now.getMonth()+1+"-"+now.getDate();
 
-                        if(now.getHours()<10){
-                            hours = "0"+now.getHours();
-                        }else{
-                            hours = now.getHours();
-                        }
-                        if(now.getMinutes()<10){
-                            minute = "0"+now.getMinutes();
-                        }else{
-                            minute = now.getMinutes();
-                        }
-                        var signtime = hours+":"+minute;
-
-                        if(scope.signStatus==="未开始"){
-
-                            $window.alert("课程未开始签到");
-                        }
-
-                        if(scope.signStatus==="正常签到"){
-                            var password = prompt("请输入签到密码：");
-                            if(password===scope.course.password){
-                                // console.log(scope.$parent);
-                                //ctrl.updateSignCnt($rootScope.user.stuno,scope.signStatus);
-                                scope.signStatus = "已签到";
-                                scope.signMark = "(正常)  签到时间 "+signtime;
-                                scope.status = 1;                  //1正常签到 -1缺勤  2迟到
-                            }
-                        }
-                        if(scope.signStatus==="迟到"){
-                            var password = prompt("请输入签到密码：");
-                            if(password===scope.course.password){
-                                // console.log(scope.$parent);
-                                //ctrl.updateSignCnt($rootScope.user.stuno,scope.signStatus);
-                                scope.signStatus = "已签到";
-                                scope.signMark = "(迟到)  签到时间 "+signtime;
-                                scope.status = 1;                  //1正常签到 -1缺勤  2迟到
-                            }
-                        }
-                        if(scope.signStatus==="缺勤"){
-                            $window.alert("已过签到时间！请准时签到..");
-                        }
-
-                    })
                 }
+                $(el).find("button").on("click",function () {
+                    var now =  new Date();
+                    var hours = "";
+                    var minute = "";
+                    var signintime = now.getYear()+"-"+now.getMonth()+1+"-"+now.getDate();
+
+                    if(now.getHours()<10){
+                        hours = "0"+now.getHours();
+                    }else{
+                        hours = now.getHours();
+                    }
+                    if(now.getMinutes()<10){
+                        minute = "0"+now.getMinutes();
+                    }else{
+                        minute = now.getMinutes();
+                    }
+                    var signtime = hours+":"+minute;
+
+                    if(scope.signState==="未开始"){
+
+                        alert("课程未开始签到");
+                    }
+                    if(scope.state==1){
+                        alert("请勿重复签到！");
+                    }
+
+                    if(scope.signState==="待签到"){
+                        var password = prompt("请输入签到密码：");
+                        if(password===scope.course.password){
+                            //ctrl.updateSignCnt($rootScope.user.stuno,scope.signStatus);
+                            scope.signState = "已签到(正常)  签到时间 "+signtime;
+
+                            scope.state = 1;//0:待签到 1:已签到 2: 迟到 3 缺勤
+                        }
+                    }
+                    if(scope.state==2){
+                        var password = prompt("请输入签到密码：");
+                        console.log(scope.course.password);
+                        if(password===scope.course.password){
+                            //ctrl.updateSignCnt($rootScope.user.stuno,scope.signStatus);
+                            scope.signState = "已签到(迟到)  签到时间"+signtime;
+                            scope.state = 1;//0:已签到 1:待签到 2: 迟到 3 缺勤
+                            console.log(signtime);
+
+                        }
+                    }
+                    if(scope.signState==="缺勤"){
+                        alert("已过签到时间！请准时签到..");
+                    }
+
+                })
 
             }
         }
